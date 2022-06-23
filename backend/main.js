c460f73cf92b24dfeb651276af793aab6f0706a8
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-06-22 23:03:43 -0400

    🎉 Simple Boilerplate Code

--- backend/main.js
+++ backend/main.js
@@ -4,3 +4,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('./config/');
+
+app.get('/', (req, res) => {
+  res.status(200).json({ code: 200, message: 'OK' });
+});
+
+app.listen(config.backend.port)
