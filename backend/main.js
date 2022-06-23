b5e8b4de375ac4b88aebcd2a015f61e567b7ea86
voidx999 <55510116+voidx999@users.noreply.github.com>
2022-06-22 23:05:27 -0400

    🐛 bugfix config dir

--- backend/main.js
+++ backend/main.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./config/');
+const config = require('../config/');
 
 app.get('/', (req, res) => {
   res.status(200).json({ code: 200, message: 'OK' });
